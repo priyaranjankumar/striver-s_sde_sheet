19e2347313c0e9edc01e4cde45dd911f7ff208a8
Priya Ranjan Kumar <mishrasubham00@gmail.com>
2023-09-11 10:21:16 +0530

    completed till pattern 14

--- pattern/Pattern14.java
+++ pattern/Pattern14.java
@@ -1,5 +1,18 @@
 package pattern;
 
 public class Pattern14 {
-    
+    public static void main(String[] args) {
+        // write your code here
+        printPattern(Integer.parseInt(args[0]));
+    }
+
+    static void printPattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print((char) (65 + j));
+            }
+            System.out.println();
+        }
+    }
+
 }
