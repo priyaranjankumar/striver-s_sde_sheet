7ca9ddc4470a1a18765cc5502269e9c2354ba780
Priya Ranjan Kumar <mishrasubham00@gmail.com>
2023-10-04 21:19:53 +0530

    agrumentsissuefixed

--- scripts/replaceheader.c
+++ scripts/replaceheader.c
@@ -20,6 +20,7 @@ int main(int argc, char* argv[]) {
     printf("argc: %d\n", argc);
     printf("argv[0]: %s\n", argv[0]);
     printf("argv[1]: %s\n", argv[1]);
+    printf("argv[2]: %s\n", argv[2]);
     if (argc != 3) {
         perror("Provide all arguments");
         return 1;
@@ -28,35 +29,35 @@ int main(int argc, char* argv[]) {
         perror("File name must not be empty");
         return 1;
     }
-    if (argv[1][0] == '\0') {
+    if (argv[2][0] == '\0') {
         perror("Date must not be empty");
         return 1;
     }
-    if (strlen(argv[1]) != 10) {
+    if (strlen(argv[2]) != 10) {
         perror("Date must be 10 characters long");
         return 1;
     }
-    if (argv[1][4] != '-' || argv[1][7] != '-') {
+    if (argv[2][4] != '-' || argv[2][7] != '-') {
         perror("Date must be in the format YYYY-MM-DD");
         return 1;
     }
-    if (argv[1][0] < '0' || argv[1][0] > '9' ||
-        argv[1][1] < '0' || argv[1][1] > '9' ||
-        argv[1][2] < '0' || argv[1][2] > '9' ||
-        argv[1][3] < '0' || argv[1][3] > '9' ||
-        argv[1][5] < '0' || argv[1][5] > '9' ||
-        argv[1][6] < '0' || argv[1][6] > '9' ||
-        argv[1][8] < '0' || argv[1][8] > '9' ||
-        argv[1][9] < '0' || argv[1][9] > '9') {
+    if (argv[2][0] < '0' || argv[2][0] > '9' ||
+        argv[2][1] < '0' || argv[2][1] > '9' ||
+        argv[2][2] < '0' || argv[2][2] > '9' ||
+        argv[2][3] < '0' || argv[2][3] > '9' ||
+        argv[2][5] < '0' || argv[2][5] > '9' ||
+        argv[2][6] < '0' || argv[2][6] > '9' ||
+        argv[2][8] < '0' || argv[2][8] > '9' ||
+        argv[2][9] < '0' || argv[2][9] > '9') {
         perror("Date must be in the format YYYY-MM-DD");
         return 1;
     }
-    if (argv[1][5] != '0' && argv[1][5] != '1') {
+    if (argv[2][5] != '0' && argv[2][5] != '1') {
         perror("Month must be between 01 and 12");
         return 1;
     }
     // Open the file in read-write mode
-    FILE *file = fopen(argv[0], "r+");
+    FILE *file = fopen(argv[1], "r+");
     if (file == NULL) {
         perror("Failed to open file");
         return 1;
@@ -74,7 +75,7 @@ int main(int argc, char* argv[]) {
     // For example, replace "old" with "new"
     //Take input
     char *old = "001|";
-    char *date = "argv[1]";
+    char *date = argv[2];
     char new[14]="";
     char *pos = strstr(header, old);
     printf("pos: %s\n", pos);
