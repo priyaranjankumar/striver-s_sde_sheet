9407a226755b6d7cd5a571bbe198a91f15b0bc40
Priya Ranjan Kumar <mishrasubham00@gmail.com>
2023-10-06 16:59:05 +0530

    added comments

--- scripts/updateheader.ps1
+++ scripts/updateheader.ps1
@@ -1,9 +1,33 @@
+<#
+
+.SYNOPSIS
+Updates the header of all CSV files in a specified folder with a specified date.
+Author - PRIYA RANJAN KUMAR 
+Date - 2021-10-20
+
+.DESCRIPTION
+This script updates the header of all CSV files in a specified folder with a specified date. If no date is provided, the current system date is used. The script uses a separate executable file called "replaceheader.exe" to replace the header of each CSV file.
+
+.PARAMETER folder
+The folder path where the CSV files are located.
+
+.PARAMETER date
+The date to use in the updated header. Should be in the format 'yyyy-MM-dd'.
+
+.EXAMPLE
+./updateheSYNOPSISader.ps1 /home/user/Documents/CSVs 2022-01-01
+Updates the header of all CSV files in the /home/user/Documents/CSVs folder with the date '2022-01-01'.
+
+.NOTES
+This script requires the "replaceheader.exe" executable file to be located in the same directory as the script file.
+#>
 
 # Check if a folder path argument was provided
 if ($args[0]) {
     # If a folder path argument was provided, use it
     $folder = $args[0]
-} else {
+}
+else {
     # If no folder path argument was provided, prompt the user for a folder path
     Write-Host "Error: No argument provided."
     return
@@ -18,12 +42,14 @@ if ($args[1]) {
     if ([DateTime]::TryParseExact($dateString, $format, $null, [System.Globalization.DateTimeStyles]::None, [ref]$date)) {
         # If the date is valid, use it
         Write-Host "Valid date: $($date.ToString($format))"
-    } else {
+    }
+    else {
         # If the date is invalid, stop the script
         Write-Host "Error: Invalid date argument provided. Please use the format 'yyyy-MM-dd'."
         return
     }
-} else {
+}
+else {
     # If no date argument was provided, use the current system date
     
     $date = Get-Date
@@ -37,5 +63,3 @@ foreach ($file in $files) {
     Write-Host $file.FullName
     & "./replaceheader.exe" $file.FullName $dateString
 }
-# Display the folder path and date
-
